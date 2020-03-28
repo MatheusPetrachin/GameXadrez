6f3ddd90ba144ca41a36998bd41366fe4a02a81e
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-03-28 11:52:50 -0300

    Criando a Classe Posicao

--- GameXadrez/Program.cs
+++ GameXadrez/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using tabuleiro;
 
 namespace GameXadrez
 {
@@ -6,7 +7,7 @@ namespace GameXadrez
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Posicao P;
         }
     }
 }
