c0732b6380239824c04a7c7de5ec913112da1518
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-03-28 12:29:45 -0300

    Inicio colocar peca tabuleiro

--- GameXadrez/tabuleiro/Peca.cs
+++ GameXadrez/tabuleiro/Peca.cs
@@ -7,11 +7,12 @@
         public int qtdMovimentos { get; protected set; }
         public Tabuleiro tab { get; protected set; }
 
-        public Peca(Posicao posicao, Cor cor, Tabuleiro tab)
+        public Peca(Tabuleiro tab, Cor cor)
         {
-            this.posicao = posicao;
+            this.posicao = null;
             this.cor = cor;
             this.tab = tab;
+            this.qtdMovimentos = 0;
         }
     }
 }
