011f76a935778774203b5ad99c19febcee245c27
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-03-28 13:27:25 -0300

    Melhorando Impressao Tabuleiro

--- GameXadrez/Program.cs
+++ GameXadrez/Program.cs
@@ -13,8 +13,9 @@ namespace GameXadrez
                 Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.colocarPeca(new Rei(tab, Cor.Branca), new Posicao(2, 6));
+                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
 
                 Tela.imprimirTabuleiro(tab);
             }
