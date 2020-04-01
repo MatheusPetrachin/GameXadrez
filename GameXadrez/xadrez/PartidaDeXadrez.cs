f6165df7eec6a7a45b729a3e2558347a28d3d5e5
MatheusPetrachin <mrp.petrachin@gmail.com>
2020-04-01 17:26:22 -0300

    promocao

--- GameXadrez/xadrez/PartidaDeXadrez.cs
+++ GameXadrez/xadrez/PartidaDeXadrez.cs
@@ -139,6 +139,22 @@ namespace xadrez
                 throw new TabuleiroException("Você não pode se colocar em Xeque!");
             }
 
+            Peca p = tab.peca(destino);
+
+            // promocao - trocar peao por uma peca melhor
+            if(p is Peao)
+            {
+                if ((p.cor == Cor.Branca && destino.linha == 0) || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
+
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
                 xeque = true;
@@ -157,9 +173,7 @@ namespace xadrez
                 turno++;
                 mudaJogador();
             }
-
-            Peca p = tab.peca(destino);
-
+                        
             // enPassant
             if (p is Peao && (destino.linha == origem.linha - 2 || destino.linha == origem.linha - 2))
             {
